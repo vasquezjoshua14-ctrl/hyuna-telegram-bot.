55a25764ffcaba514610ced121f64ca62b690651
vasquezjoshua14-ctrl <vasquezjoshua14@gmail.com>
2026-09-08 12:10:12 +0300

    Fix syntax for parse_mode in bot.js

--- bot.js
+++ bot.js
@@ -214,10 +214,9 @@ bot.action('payment_guide', async (ctx) => {
   '3. Once payment is confirmed, your order status will update automatically.\n' +
   '4. Manual products will be prepared and delivered here.\n\n' +
   '💗 Keep your Order ID for reference.',
-  {
-    parse_mode: 'Markdown',
-    }
+  { parse_mode: 'Markdown' }
 )
+})
 
 bot.action('my_orders', async (ctx) => {
   await ctx.answerCbQuery()
