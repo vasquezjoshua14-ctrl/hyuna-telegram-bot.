729c8697a6a5e59f3ca4cdc79a49c825dae80b13
vasquezjoshua14-ctrl <vasquezjoshua14@gmail.com>
2026-09-10 07:46:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -362,7 +362,7 @@ bot.action("payment_guide", async (ctx) => {
 
   await ctx.replyWithPhoto(
     { source: "./GCash-MyQR-08092026123724.PNG.jpg" },
-    {
+    {,
       caption,
       parse_mode: "Markdown",
       ...(order
